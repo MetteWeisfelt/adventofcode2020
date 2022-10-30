cb6f8802f71c45093612e0474921ca98fa6349ce
MetteWeisfelt <metteweisfelt@gmail.com>
2022-10-30 18:35:31 +0100

    set structure for day 2

---
+++ days/DayTwo.java
@@ -0,0 +1,19 @@
+public class DayTwo {
+    public static void runchallenge1(){
+        System.out.println(">>> day 2 challenge 1 is starting now ...");
+        challenge1();
+    }
+
+    public static void runchallenge2(){
+        System.out.println(">>> day 2 challenge 2 is starting now ...");
+        challenge2();
+    }
+
+    public static void challenge1(){
+
+    }
+
+    public static void challenge2(){
+        
+    }
+}
--- days/Start.java
+++ days/Start.java
@@ -1,6 +1,6 @@
 public class Start {
     public static void main(String[] args){
-        DayOne.runchallenge1();
-        DayOne.runchallenge2();
+        DayTwo.runchallenge1();
+        DayTwo.runchallenge2();
     }
 }
